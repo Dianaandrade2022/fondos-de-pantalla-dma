e689f87173890c383b1909e4aa7ce4289fb7ba8a
dianaandrade2022 <a3521110157@alumno.uttehuacan.edu.mx>
2024-05-31 22:30:05 -0600

    four commit

--- app/src/main/java/com/example/fondosdepantalladma/MainActivityAdministrador.java
+++ app/src/main/java/com/example/fondosdepantalladma/MainActivityAdministrador.java
@@ -1,13 +1,21 @@
 package com.example.fondosdepantalladma;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Toolbar;
+import android.widget.Toast;
 
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.fondosdepantalladma.FragmentosAdministrador.InicioAdmin;
+import com.example.fondosdepantalladma.FragmentosAdministrador.ListAdmin;
+import com.example.fondosdepantalladma.FragmentosAdministrador.PerfilAdmin;
+import com.example.fondosdepantalladma.FragmentosAdministrador.RegistrarAdmin;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivityAdministrador extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -20,11 +28,41 @@ public class MainActivityAdministrador extends AppCompatActivity implements Navi
 
         Toolbar toolbar = findViewById(R.id.toolbarA);
         setSupportActionBar(toolbar);
+
         drawerLayout = findViewById(R.id.nav_viewA);
+
+        NavigationView navigationView = findViewById(R.id.nav_viewA);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setItemIconTintList(null);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_containerA, new InicioAdmin()).commit();
+            navigationView.setCheckedItem(R.id.InicioAdmin);
+        }
+
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.InicioAdmin) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_containerA, new InicioAdmin()).commit();
+        } else if (itemId == R.id.PerfilAdmin) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_containerA, new PerfilAdmin()).commit();
+        } else if (itemId == R.id.RegistrarAdmin) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_containerA, new RegistrarAdmin()).commit();
+        } else if (itemId == R.id.ListarAdmin) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_containerA, new ListAdmin()).commit();
+        } else if (itemId == R.id.Salir) {
+            // Lógica para salir, por ejemplo, cerrando la sesión
+            Toast.makeText(this, "Cerrar sesión", Toast.LENGTH_SHORT).show();
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
