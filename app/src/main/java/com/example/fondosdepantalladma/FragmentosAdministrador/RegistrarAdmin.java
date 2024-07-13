77773bbfeab47f789c912bb303cd1b883e91c40d
dianaandrade2022 <a3521110157@alumno.uttehuacan.edu.mx>
2024-07-12 22:02:49 -0600

    today

--- app/src/main/java/com/example/fondosdepantalladma/FragmentosAdministrador/RegistrarAdmin.java
+++ app/src/main/java/com/example/fondosdepantalladma/FragmentosAdministrador/RegistrarAdmin.java
@@ -1,17 +1,46 @@
 package com.example.fondosdepantalladma.FragmentosAdministrador;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.fondosdepantalladma.MainActivityAdministrador;
 import com.example.fondosdepantalladma.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 
 
 public class RegistrarAdmin extends Fragment {
+
+    TextView FechaRegistro;
+    EditText Correo,Password,Nombres,Apellidos,Edad;
+    Button Registrar;
+
+    FirebaseAuth auth;
+
+    ProgressDialog progressDialog;
 
 
     @Override
@@ -20,7 +49,110 @@ public class RegistrarAdmin extends Fragment {
         // Inflate the layout for this fragment
 //
         View view = inflater.inflate(R.layout.fragment_registrar_admin, container, false);
+
+        FechaRegistro = view.findViewById(R.id.FechaRegistro);
+        Correo = view.findViewById(R.id.Correo);
+        Password = view.findViewById(R.id.Password);
+        Nombres = view.findViewById(R.id.Nombres);
+        Apellidos = view.findViewById(R.id.Apellidos);
+        Edad = view.findViewById(R.id.Edad);
+        Registrar = view.findViewById(R.id.Registrar);
+
+//        Inicializando firebase Authentication
+        auth = FirebaseAuth.getInstance();
+
+
+        Date date = new Date();
+        SimpleDateFormat fecha = new SimpleDateFormat("d 'de' MMMM 'del' yyyy");
+
+        String SFecha = fecha.format(date);
+
+        FechaRegistro.setText(SFecha);
+
+        Registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Convertimos a string EditText: Correo y Password
+                String correo = Correo.getText().toString();
+                String pass = Password.getText().toString();
+
+//                Validación de Correo
+
+                if (!Patterns.EMAIL_ADDRESS.matcher(correo).matches()) {
+                    Correo.setError("¡Correo inválido!");
+                    Correo.setFocusable(true);
+                } else if (pass.length() < 6) {
+                    Password.setError("La contraseña debe ser mayor o igual a 6 carácteres");
+                    Password.setFocusable(true);
+                }else{
+                    RegistroAdministradores(correo,pass);
+                }
+            }
+        });
+
+        progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setMessage("Registrando, espere ...");
+        progressDialog.setCancelable(false);
+
+
         return view;
+
+    }
+
+    private void RegistroAdministradores(String correo, String pass) {
+
+        progressDialog.show();
+        auth.createUserWithEmailAndPassword(correo,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    progressDialog.dismiss();
+                    FirebaseUser user = auth.getCurrentUser();
+                    assert user != null;
+
+                    String UID = user.getUid();
+                    String correo = Correo.getText().toString();
+                    String pass = Password.getText().toString();
+                    String nombres = Nombres.getText().toString();
+                    String apellidos = Apellidos.getText().toString();
+                    String edad = Edad.getText().toString();
+                    int EdadInt  = Integer.parseInt(edad);
+
+                    HashMap<Object,Object> Administradores = new HashMap<>();
+
+                    Administradores.put("UID", UID);
+                    Administradores.put("CORREO", correo);
+                    Administradores.put("PASS", pass);
+                    Administradores.put("NOMBRES", nombres);
+                    Administradores.put("APELLIDOS", apellidos);
+                    Administradores.put("EDAD", EdadInt);
+                    Administradores.put("IMAGEN", "");
+
+//                    Inicializar FirebaseDatabase
+
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference reference = database.getReference("Bse de datos administradores9b");
+
+                    reference.child(UID).setValue(Administradores);
+
+                    startActivity(new Intent(getActivity(), MainActivityAdministrador.class));
+                    Toast.makeText(getActivity(), "Registro Exitoso", Toast.LENGTH_SHORT).show();
+                    getActivity().finish();
+
+                }
+                else {
+                    progressDialog.dismiss();
+                    Toast.makeText(getActivity(), "Ha ocurrido un error", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getActivity(), "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
 
     }
 }
--- app/src/main/java/com/example/fondosdepantalladma/MainActivityAdministrador.java
+++ app/src/main/java/com/example/fondosdepantalladma/MainActivityAdministrador.java
@@ -1,26 +1,32 @@
 package com.example.fondosdepantalladma;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
-
-import androidx.appcompat.widget.Toolbar;
 
 import com.example.fondosdepantalladma.FragmentosAdministrador.InicioAdmin;
 import com.example.fondosdepantalladma.FragmentosAdministrador.ListAdmin;
 import com.example.fondosdepantalladma.FragmentosAdministrador.PerfilAdmin;
 import com.example.fondosdepantalladma.FragmentosAdministrador.RegistrarAdmin;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivityAdministrador extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivityAdministrador extends AppCompatActivity implements OnNavigationItemSelectedListener {
 
     DrawerLayout drawerLayout;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,34 +41,68 @@ public class MainActivityAdministrador extends AppCompatActivity implements Navi
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.setItemIconTintList(null);
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+
+        //Fragmento por defectoD
+
         if (savedInstanceState == null){
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_containerA, new InicioAdmin()).commit();
+
             navigationView.setCheckedItem(R.id.InicioAdmin);
         }
-
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int itemId = item.getItemId();
 
-        if (itemId == R.id.InicioAdmin) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_containerA, new InicioAdmin()).commit();
-        } else if (itemId == R.id.PerfilAdmin) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_containerA, new PerfilAdmin()).commit();
-        } else if (itemId == R.id.RegistrarAdmin) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_containerA, new RegistrarAdmin()).commit();
-        } else if (itemId == R.id.ListarAdmin) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_containerA, new ListAdmin()).commit();
+        if (itemId == R.id.InicioAdmin){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_containerA,
+                    new InicioAdmin()).commit();
+        } else if (itemId == R.id.PerfilAdmin){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_containerA,
+                    new PerfilAdmin()).commit();
+        }else if (itemId == R.id.RegistrarAdmin) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_containerA,
+                    new RegistrarAdmin()).commit();
+        }else if (itemId == R.id.ListarAdmin) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_containerA,
+                    new ListAdmin()).commit();
         } else if (itemId == R.id.Salir) {
-            // Lógica para salir, por ejemplo, cerrando la sesión
-            Toast.makeText(this, "Cerrar sesión", Toast.LENGTH_SHORT).show();
+            CerrarSesion();
         }
+
         drawerLayout.closeDrawer(GravityCompat.START);
-        return true;
+        return false;
+    }
+
+    private void ComprobandoInicioSesion (){
+        if(user != null){
+            // Si el administrador a iniciado sesion
+            Toast.makeText(this, "Se ha iniciado sesión ", Toast.LENGTH_SHORT).show();
+        }else{
+            //Si no ha iniciado sesión, es porque el usuario es un cliente
+            startActivity(new Intent(MainActivityAdministrador.this,MainActivity.class));
+            finish();
+        }
+    }
+
+    private void CerrarSesion(){
+        firebaseAuth.signOut(); //null
+        startActivity(new Intent(MainActivityAdministrador.this, MainActivity.class));
+        Toast.makeText(this, "Sesión cerrada exitosamente", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        ComprobandoInicioSesion();
+        super.onStart();
     }
 }
